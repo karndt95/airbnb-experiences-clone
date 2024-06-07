f2cb77d94d993c448cd42e9de9e2310ad25c1976
karndt95 <karndt1995@gmail.com>
2024-06-07 10:32:58 -0400

    Add Card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
---
+++ src/components/card/Card.js
@@ -0,0 +1,5 @@
+export default function Card() {
+  return (
+    <h1>Replace This</h1>
+  );
+}
